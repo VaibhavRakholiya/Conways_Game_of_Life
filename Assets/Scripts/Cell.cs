b8e34964a493b4cb5d23d10929eb19a5dfa6373b
VaibhavRakholiya <rakholiyavaibhav.git@gmail.com>
2022-03-22 17:23:26 +0530

    22/02/2022

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -6,32 +6,35 @@ public class Cell : MonoBehaviour
 {
     public int Neighbours;
     public bool isalive;
+    public Color TurnOff_Color,TurnOn_Color;
+    public int index_i, index_j;
     private SpriteRenderer sprite;
     private void Awake()
     {
         sprite = this.GetComponent<SpriteRenderer>();
-        isalive = true;
+        sprite.color = TurnOff_Color;
+        isalive = false;
     }
-    void Start()
-    {
-    }
+   
 
-    // Update is called once per frame
-    void Update()
+    public void setFalse()
     {
-        
+        isalive = false;
+        sprite.color = TurnOff_Color;
     }
     public void ToggleCell()
     {
         if(isalive)
         {
             isalive = false;
-            sprite.enabled = false;
+            sprite.color = TurnOff_Color;
+            //sprite.enabled = false;
         }
         else
         {
             isalive = true;
-            sprite.enabled = false;
+            sprite.color = TurnOn_Color;
+            //sprite.enabled = true;
         }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public int PatternNo=-1;
     public GameObject cell_prefab;
-    public int grid_Width;
-    public int grid_Height;
+    public int grid_Size=5;
     public Cell[,] grid;
     private void Awake()
     {
@@ -15,25 +15,85 @@ public class GameManager : MonoBehaviour
     }
     private void Start()
     {
-        CreateGrid();
+        CreateGrid(grid_Size);
+        StartCoroutine(LifeCycle());
     }
     private void Update()
     {
+       if(Input.GetMouseButtonDown(0))
+        {
+            DrawPattern();
+        }
+    }
+    private void DrawPattern()
+    {
+        if (PatternNo > 0)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit) )
+            {
+                int i = hit.collider.GetComponent<Cell>().index_i;
+                int j = hit.collider.GetComponent<Cell>().index_j;
+                if (PatternNo == 1)
+                {
+                    // Draw Oscilator on Grid.
+                    if(j-1 > 0)
+                    grid[i, j - 1].ToggleCell();
+                    grid[i, j].ToggleCell();
+                    if(j+1 < grid_Size)
+                    grid[i, j + 1].ToggleCell();
+                }
+                else if(PatternNo == 2)
+                {
+                    // Draw Block on Grid.
+                    if(i+1 < grid_Size)
+                    grid[i+1, j].ToggleCell();
+                    if(j+1 <grid_Size)
+                    grid[i,j+1].ToggleCell();
+                    grid[i, j].ToggleCell();
+                }
+                else if (PatternNo == 3)
+                {
+                    // Draw Glider on Grid.
+                    TurnOnCell(i, j);
+                    TurnOnCell(i+1, j-1);
+                    TurnOnCell(i+1, j-2);
+                    TurnOnCell(i, j-2);
+                    TurnOnCell(i-1, j-2);
+
+                }
+
+            }
+        }
+    }
+    private void TurnOnCell(int i,int j)
+    {
+        if(i>0 && i < grid_Size && j>0 && j < grid_Size)
+        {
+            grid[i, j].ToggleCell();
+        }
+    }
+    private IEnumerator LifeCycle()
+    {
         CheckNeighbours();
+        LifeControl();
+        yield return new WaitForSeconds(0.50f);
+        StartCoroutine(LifeCycle());
     }
     private void CheckNeighbours()
     {
-        for (int i = 0; i < grid_Height; i++)
+        for (int i = 0; i < grid_Size; i++)
         {
-            for (int j = 0; j < grid_Width; j++)
+            for (int j = 0; j < grid_Size; j++)
             {
                 int neighbours = 0;
-                if(i+1<grid_Height)
+                if(i+1<grid_Size)
                 {
                     if (grid[j, i + 1].isalive)
                         neighbours++;
                 }
-                if(j+1<grid_Width)
+                if(j+1<grid_Size)
                 {
                     if (grid[j + 1, i].isalive)
                         neighbours++;
@@ -48,19 +108,19 @@ public class GameManager : MonoBehaviour
                     if (grid[j - 1, i].isalive)
                         neighbours++;
                 }
-                if(j+1 < grid_Height && i+1 < grid_Width)
+                if(j+1 < grid_Size && i+1 < grid_Size)
                 {
                     if (grid[j + 1, i + 1].isalive)
                         neighbours++;
                 }
-                if(j-1 >=0 && i+1 <grid_Height)
+                if(j-1 >=0 && i+1 <grid_Size)
                 {
                     if(grid[j-1,i+1].isalive)
                     {
                         neighbours++;
                     }
                 }
-                if(j+1 < grid_Width && i-1>=0)
+                if(j+1 < grid_Size && i-1>=0)
                 {
                     if(grid[j+1,i-1].isalive)
                     {
@@ -78,18 +138,62 @@ public class GameManager : MonoBehaviour
             }
         }
     }
-    private void CreateGrid()
+    private void LifeControl()
     {
-        grid = new Cell[grid_Height, grid_Width];
-        for(int i=0;i<grid_Height;i++)
+        for (int i=0;i<grid_Size;i++)
         {
-            for(int j=0;j<grid_Width;j++)
+            for(int j=0;j<grid_Size;j++)
+            {
+                    if(grid[j,i].isalive)
+                    {
+                            if(grid[j,i].Neighbours != 2 && grid[j,i].Neighbours !=3)
+                            {
+                                 grid[j, i].ToggleCell();
+                            }
+                    }
+                    else
+                    {
+                        if(grid[j,i].Neighbours == 3)
+                        {
+                                grid[j, i].ToggleCell();
+                        }
+                    }
+            }
+        }
+    }
+    public void CreateGrid(int _grid_Size)
+    {
+        grid_Size = _grid_Size;
+        UIManager.instance.CurrentGrid_Size_Text.text = "Grid Size : " + grid_Size;
+        Camera.main.transform.position = new Vector3(grid_Size, grid_Size/2, -10f);
+        Camera.main.orthographicSize = (grid_Size + grid_Size) / 2.5f;
+        grid = new Cell[grid_Size, grid_Size];
+        for(int i=0;i<grid_Size;i++)
+        {
+            for(int j=0;j<grid_Size;j++)
             {
                 GameObject cell_gameObject = Instantiate(cell_prefab, new Vector2(i, j), Quaternion.identity);
                 Cell cell = cell_gameObject.GetComponent<Cell>();
-                RandomlysetAlive(cell);
+                cell.index_i = i;
+                cell.index_j = j;
                 grid[i, j] = cell;
+                RandomlysetAlive(cell);
             }
+        }
+    }
+    public void ClearGrid()
+    {
+        foreach (Cell item in grid)
+        {
+            item.setFalse();
+        }
+    }
+    public void DestroyGrid()
+    {
+
+        foreach (Cell item in grid)
+        {
+            Destroy(item.gameObject);
         }
     }
     private void RandomlysetAlive(Cell cell)
